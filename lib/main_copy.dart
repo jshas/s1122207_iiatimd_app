27d75ad9f9b921e4fba2a1c291504b817e9ed918
jshas <62602628+jshas@users.noreply.github.com>
2023-07-16 14:44:10 +0200

    Theme Switching functionality added, works fully. __ Added basic BLoC dart __ Added ActivitiesPage and TimersPage placeholders TODO: Fix landscape-orientation placing of button label

--- lib/main_copy.dart
+++ lib/main_copy.dart
@@ -8,7 +8,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This wiydget is the root of your application.
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/presentation/screen/settings_screen.dart
+++ lib/presentation/screen/settings_screen.dart
@@ -29,8 +29,9 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 direction: Axis.vertical,
                 children: [
                   ListTile(
+                    titleAlignment: ListTileTitleAlignment.threeLine,
                     title: Text("Theme",
-                        style: Theme.of(context).textTheme.bodyLarge),
+                        style: Theme.of(context).textTheme.bodyMedium),
                     trailing: SegmentedButton(segments: const [
                       ButtonSegment<ThemeItem>(
                           value: ThemeItem.light,
@@ -41,7 +42,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
                           value: ThemeItem.dark,
                           enabled: true,
                           icon: Icon(Icons.dark_mode),
-                          label: Text("dark")),
+                          label: Text("Night")),
                       ButtonSegment<ThemeItem>(
                           value: ThemeItem.system,
                           enabled: true,
@@ -57,42 +58,42 @@ class _SettingsScreenState extends State<SettingsScreen> {
                   )
                 ],
               ),
-              Flex(
-                direction: Axis.horizontal,
-                children: [
-                  Flexible(
-                    flex: 1,
-                    fit: FlexFit.tight,
-                    child: ListTile(
-                      visualDensity: VisualDensity.adaptivePlatformDensity,
-                      title: Text("Reminder timer duration", style: Theme.of(context).textTheme.bodyLarge),
-                      trailing: SegmentedButton(segments: const [
-                        ButtonSegment<ThemeItem>(
-                            value: ThemeItem.light,
-                            enabled: true,
-                            icon: Icon(Icons.light_mode),
-                            label: Text("Light")),
-                        ButtonSegment<ThemeItem>(
-                            value: ThemeItem.dark,
-                            enabled: true,
-                            icon: Icon(Icons.dark_mode),
-                            label: Text("dark")),
-                        ButtonSegment<ThemeItem>(
-                            value: ThemeItem.system,
-                            enabled: true,
-                            icon: Icon(Icons.android),
-                            label: Text("Follow OS")),
-                      ], selected: <ThemeItem>{selection},
-                        onSelectionChanged: (Set<ThemeItem>selectedThemeItem) {
-                          context.read<ThemeCubit>().setTheme(
-                              selectedThemeItem.first);
-                          selection = selectedThemeItem.first;
-                        },
-                      ),
-                    ),
-                  )
-                ],
-              )
+              // Flex(
+              //   direction: Axis.horizontal,
+              //   children: [
+              //     Flexible(
+              //       flex: 1,
+              //       fit: FlexFit.tight,
+              //       child: ListTile(
+              //         visualDensity: VisualDensity.adaptivePlatformDensity,
+              //         title: Text("Reminder timer duration", style: Theme.of(context).textTheme.bodyLarge),
+              //         trailing: SegmentedButton(segments: const [
+              //           ButtonSegment<ThemeItem>(
+              //               value: ThemeItem.light,
+              //               enabled: true,
+              //               icon: Icon(Icons.light_mode),
+              //               label: Text("Light")),
+              //           ButtonSegment<ThemeItem>(
+              //               value: ThemeItem.dark,
+              //               enabled: true,
+              //               icon: Icon(Icons.dark_mode),
+              //               label: Text("dark")),
+              //           ButtonSegment<ThemeItem>(
+              //               value: ThemeItem.system,
+              //               enabled: true,
+              //               icon: Icon(Icons.android),
+              //               label: Text("Follow OS")),
+              //         ], selected: <ThemeItem>{selection},
+              //           onSelectionChanged: (Set<ThemeItem>selectedThemeItem) {
+              //             context.read<ThemeCubit>().setTheme(
+              //                 selectedThemeItem.first);
+              //             selection = selectedThemeItem.first;
+              //           },
+              //         ),
+              //       ),
+              //     )
+              //   ],
+              // )
             ],
 
           ),
